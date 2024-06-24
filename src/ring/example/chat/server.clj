a8204f1ab96819d69e187dd00019d8a79704a97a
Hiroshi Kimura <hiroshi.kimura.0331@gmail.com>
2024-06-24 10:01:58 +0900

    added /index

--- src/ring/example/chat/server.clj
+++ src/ring/example/chat/server.clj
@@ -1,42 +1,62 @@
 (ns ring.example.chat.server
   (:require [clojure.core.async :as a]
+            [clojure.java.io :as io]
             [reitit.ring :as rr]
             [ring.adapter.jetty :as adapter]
+            [ring.util.anti-forgery :refer [anti-forgery-field]]
+            [ring.middleware.defaults :as def]
+            [ring.util.response :as resp]
             [ring.websocket.async :as wsa]
             [ring.websocket.transit :as wst]
-            [ring.websocket.keepalive :as wska]))
+            [ring.websocket.keepalive :as wska]
+            [taoensso.telemere :as t]))
 
-;; In this function we define a channel to be the main writer, and a 'mult' to
-;; distribute the values placed on the channel to multiple readers.
-;;
-;; When a WebSocket is opened, we 'tap' the mult with the WebSocket's output
-;; channel, and pipe the WebSocket's input channel to the writer channel. This
-;; will take any input from this WebSocket, and output it to all WebSockets
-;; currently connected to the server.
 (defn make-chat-handler []
   (let [writer  (a/chan)
         readers (a/mult writer)]
     (fn handler [_request]
       (wsa/go-websocket [in out]
-        (a/tap readers out)
-        (a/pipe in writer false)))))
+                        (a/tap readers out)
+                        (a/pipe in writer false)))))
 
-;; We use Reitit for the routes, defining a "/chat" route for the websocket,
-;; and a resource handler to serve up the static files, such as the index,
-;; the compile ClojureScript and CSS styling.
-;;
-;; We'll also add in two middleware functions: one to parse the WebSocket input
-;; using Transit, a Clojure/Script library for efficiently passing Clojure data
-;; structures from JavaScript. The other middleware will periodically ping the
-;; client to prevent it from timing out ('keepalive').
+(defn login [_request]
+  (-> (resp/response
+       (str
+        "<!DOCTYPE html><title>MX3</title><h1>Micro X versin 3</h1>"
+        "<form method='post'>"
+        (anti-forgery-field)
+        "<input name='login'>
+         <input name='password' type='password'>
+         <input type='submit'>
+         </form>"))
+      (resp/content-type "text/html")
+      (resp/charset "UTF-8")))
+
+(defn login! [{{:keys [login password]} :params :as request}]
+  (t/log! {:id "login"} [login password (:params request)])
+  (let [ret (-> {:status 303
+                 :headers {"location" "/"}}
+                (assoc-in [:session :identity] login))]
+    (t/log! :info ["ret" ret])
+    ret))
+
+(defn index [request]
+  (-> (resp/response (slurp (io/resource "public/index")))
+      (resp/content-type "text/html")
+      (resp/charset "UTF-8")))
+
 (defn make-app-handler []
   (rr/ring-handler
-   (rr/router ["/chat" (make-chat-handler)])
+   (rr/router [["/chat" {:middleware [[wst/wrap-websocket-transit]
+                                      [wska/wrap-websocket-keepalive]]}
+                ["" (make-chat-handler)]]
+               ["" {:middleware [[def/wrap-defaults def/site-defaults]]}
+                ["/login" {:get login :post login!}]
+                ["/index" index]]])
    (rr/routes
     (rr/create-resource-handler {:path "/"})
     (rr/create-default-handler))
-   {:middleware [[wst/wrap-websocket-transit]
-                 [wska/wrap-websocket-keepalive]]}))
+   {:middleware []}))
 
 (defn run-server [options]
   (adapter/run-jetty (make-app-handler) options))
