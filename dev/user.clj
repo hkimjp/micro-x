24b90e5ae6b7618538a7f2be9120d134de542037
hkimjp <hkim@hkim.jp>
2025-05-06 16:58:19 +0900

    under construction

--- dev/user.clj
+++ dev/user.clj
@@ -1,9 +1,8 @@
 (ns user
   (:require [taoensso.telemere :as t]
-   [chat.server :as s]
-   [hato.client :as hc]))
+            [chat.server :as s]
+            [hato.client :as hc]))
 
 (t/set-min-level! :debug)
 
-(s/stop)
 
--- src/chat/async.clj
+++ src/chat/async.clj
@@ -2,7 +2,7 @@
   (:require [clojure.core.async :as a]
             [ring.websocket :as ws]
             [ring.websocket.protocols :as wsp]
-            ; [java-time.api :as jt]
+            [java-time.api :as jt]
             [taoensso.telemere :as t]
             [hkimjp.datascript :as ds]))
 
@@ -43,8 +43,7 @@
       ;; to compare timestamps, timestamp must not be strings.
       ;; need improve.
       (t/log! :info mesg)
-      ;(ds/put (assoc mesg :db/add -1 :timestamp (jt/local-date-time)))
-      (ds/put (assoc mesg :db/add -1 :timestamp (java.util.Date.)))
+      (ds/put (assoc mesg :db/add -1 :timestamp (jt/local-date-time)))
       (a/put! in mesg))
     (on-pong [_ _ _]
       ; (t/log! :info "on-pong")
